31856d2f4ad3750a9c82ceb2f03fe58d4234b659
Kennedy Miyake <kennedykaiak57@gmail.com>
2023-02-18 18:59:33 -0300

    Project Market in C

---
+++ miitest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Olá! Seja bem-vindo(a) a minha interação!\n");
+    //sleep(1);
+    printf("Por favor, informe seu nome para que possamos começar.\n");
+    //sleep(1);
+
+    char name[100];
+    int nameLength = strlen(name);
+    printf("Digite seu nome: ");
+    scanf("%s", name);
+
+    for (int i = 0; i < nameLength; i++){
+        if (ispunct(name[i]) || isdigit(name[i]) || !isspace(name[i])){
+            printf("Por favor, digite um nome válido.\n");
+            return 1;
+        }
+    }
+    printf("Olá, %s!", name);
+}
